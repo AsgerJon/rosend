e16e6af74bb24d238dd7d6eb6964ac33e6621058
Asger Jon Vistisen <asgerjon2@gmail.com>
2024-01-25 16:21:30 +0100

    Initial commit

---
+++ src/rosend/_get_topics.py
@@ -0,0 +1,4 @@
+"""Getter function for list of published topics"""
+#  MIT Licence
+#  Copyright (c) 2024 Asger Jon Vistisen
+from __future__ import annotations
---
+++ src/rosend/_ros_type.py
@@ -0,0 +1,5 @@
+"""RosType wraps the std_msgs class in code compatible with the companion
+application environment. """
+#  MIT Licence
+#  Copyright (c) 2024 Asger Jon Vistisen
+from __future__ import annotations
